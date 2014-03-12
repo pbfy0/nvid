9e9bbcd207a612234dfe9f768ebc1bb54fdcb646
pbfy0 <pbfy00@gmail.com>
2014-03-11 21:53:40 -0400

    .tns

--- converter/wrapper.bat
+++ converter/wrapper.bat
@@ -1,3 +1,3 @@
 cd %~dp0
-.\ffmpeg.exe -i "%1" -r 24 -t 60 -vcodec libvpx -vf "scale='if(gt(a,4/3),320,-1)':'if(gt(a,4/3),-1,240)',pad=320:240:(320-in_w)/2:(240-in_h)/2" output.ivf
+.\ffmpeg.exe -i "%1" -r 24 -t 60 -vcodec libvpx -vf "scale='if(gt(a,4/3),320,-1)':'if(gt(a,4/3),-1,240)',pad=320:240:(320-in_w)/2:(240-in_h)/2" output.ivf.tns
 pause
